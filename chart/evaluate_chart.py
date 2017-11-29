25a3646e0ddb2708886a87554312c08d9b2fdec8
Charles-Edwin <charles-edwin.debrouwer@student.uclouvain.be>
2017-11-29 17:25:39 +0100

    Correction bar chart is working

--- chart/evaluate_chart.py
+++ chart/evaluate_chart.py
@@ -15,11 +15,28 @@ import re
 
 
 def evaluate_chart(question, response):
+    '''
+    :param question: a Question object
+    :param response: The response to assess
+    :return: 1 if the response is correct, 0 if incorrect, -1 if automatic evaluation is impossible
+    '''
     raw_correct_answers = question.get_answer()
     evaluation_type = raw_correct_answers["type"]
+    answer_json = json.loads(raw_correct_answers['answers'][0]['chart'])
+
 
     if evaluation_type == "chart-barchart":
-        #TODO
-        return 1
+        student_answers = []
+        for x in response[0].decode('utf-8').split(','):
+            student_answers.append(int(x))
+        correct_answers = answer_json['point']
+        if len(student_answers) != len(correct_answers):
+            return 0
+        score = 0
+        for (student_answer, correct_answer) in zip(student_answers,correct_answers):
+            if student_answer == correct_answer:
+                score += 1
+        # raise Exception('1' if score == len(correct_answers) else '0')
+        return 1 if score == len(correct_answers) else 0
     else:
         return 1

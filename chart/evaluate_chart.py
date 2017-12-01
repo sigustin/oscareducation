cd34b88f0c5ab5d07fa4eb6f206b6cd47526465e
Charles-Edwin <charles-edwin.debrouwer@student.uclouvain.be>
2017-12-01 18:25:31 +0100

    test for the correction of the barchart

--- chart/evaluate_chart.py
+++ chart/evaluate_chart.py
@@ -51,35 +51,37 @@ def evaluate_chart(question, response):
     :param response: The response to assess
     :return: 1 if the response is correct, 0 if incorrect, -1 if automatic evaluation is impossible
     '''
-    raw_correct_answers = question.get_answer()
-    evaluation_type = raw_correct_answers["type"]
-    list_raw_correct_answers = raw_correct_answers['answers']
-    #answer_json = json.loads(raw_correct_answers['answers'][0]['chart'])
+    try:
+        raw_correct_answers = question.get_answer()
+        evaluation_type = raw_correct_answers["type"]
+        list_raw_correct_answers = raw_correct_answers['answers']
+        #answer_json = json.loads(raw_correct_answers['answers'][0]['chart'])
 
-
-    if evaluation_type == "chart-barchart":
-        student_answers = []
-        for x in response[0].decode('utf-8').split(','):
-            student_answers.append(int(x))
-        raise Exception('response len ='+str(len(response))+'  '+str(response))
-        for answer_str in list_raw_correct_answers:
-            answer_json = json.load(answer_str)
-            correct_answer = answer_json['point']
-            if sameArray(correct_answer, student_answers):
-                return 1
-        return 0
-    # elif evaluation_type == "chart-piechart":
-    #     #Assuming answer are in a dic key are sectorname and value are sector size
-    #     student_answers = {}
-    #
-    #     student_answers = sorted(student_answers.items(), key=lambda x: (x[1],x[0]),reverse=True)
-    #     for answer_str in list_raw_correct_answers:
-    #         answer_json = json.load(answer_str)
-    #         correct_answer = sorted(answer_json['point'].items(), key=lambda x: (x[1],x[0]),reverse=True)
-    #
-    #         raise Exception('Need more information')
-    #         if sameDic(correct_answer, student_answers):
-    #             return 1
-    #     return 0
-    else:
+        if evaluation_type == "chart-barchart":
+            student_answers = []
+            for x in response[0].decode('utf-8').split(','):
+                student_answers.append(int(x))
+            # raise Exception('response len ='+str(len(response))+'  '+str(response))
+            for answer_str in list_raw_correct_answers:
+                answer_json = json.loads(str(answer_str['chart']))
+                correct_answer = answer_json['point']
+                if sameArray(correct_answer, student_answers):
+                    return 1
+            return 0
+        # elif evaluation_type == "chart-piechart":
+        #     #Assuming answer are in a dic key are sectorname and value are sector size
+        #     student_answers = {}
+        #
+        #     student_answers = sorted(student_answers.items(), key=lambda x: (x[1],x[0]),reverse=True)
+        #     for answer_str in list_raw_correct_answers:
+        #         answer_json = json.load(answer_str)
+        #         correct_answer = sorted(answer_json['point'].items(), key=lambda x: (x[1],x[0]),reverse=True)
+        #
+        #         raise Exception('Need more information')
+        #         if sameDic(correct_answer, student_answers):
+        #             return 1
+        #     return 0
+        else:
+            return -1
+    except ValueError:
         return -1
---
+++ chart/tests.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Add your tests in this file
+# pyhamcrest has to be installed
+# doc: http://pyhamcrest.readthedocs.io/en/release-1.8/tutorial/
+
+import unittest
+# from selenium import webdriver
+# from selenium.webdriver.comm.keys import Keys
+# from selenium.webdriver.support.ui import Select
+# from selenium.webdriver.support import WebDriverWait
+from chart.evaluate_chart import evaluate_chart
+from examinations.models import Question
+from django.test import TestCase
+
+
+# class TeacherSide(unittest.TestCase):
+#
+# 	def setUp(self):
+# 		self.driver = webdriver.Firefox()
+# 		driver = self.driver
+# 		driver.get("http://127.0.01:8000")
+# 		wait = WebDriverWait(driver, 10)
+# 		#assert "No results found." not in driver.page_source
+#
+# 	def tearDown(self):
+# 		self.driver.close()
+
+
+class CorrectionChartExercices(TestCase):
+    def setUp(self):
+        commonQparts = ["type: ","""\nanswers:\n- chart: '{"point":""",
+						""","AxisX":"abscisses","AxisY":"ordonnees","zeroX":-1,"zeroY":-1,"maxX":10,"maxY":10}'"""]
+        setOfTest = [['Q0','chart-barchart', '[1,1,1,2]', [u'1,1,2'], 0,'response of different lenght shouldnt be considere as a True answert'],
+					 ['Q1','chart-barchart', '[1,1,1,2]', [u'1,1,1,2'], 1,'same answert should be True'],
+                     ['Q2','chart-barchart','[2,2,2,2,2]',[u'1,1,2,2,2'],0,'answert different but of same length'],
+                     ['Q3','chart-barchart','',[u'1,2,1'],-1,'without true answer, it is impossible to correct']]
+        self.studentAnswer = []
+        self.output = []
+        self.message = []
+        for test in setOfTest:
+            Question.objects.create(description=test[0],answer=commonQparts[0]+test[1]+commonQparts[1]+test[2]+
+                                                               commonQparts[2],source="None",indication=test[1])
+            self.studentAnswer.append(test[3])
+            self.output.append(test[4])
+            self.message.append(test[5])
+
+    def testBarchart(self):
+        for i in range(len(self.studentAnswer)):
+            Q = Question.objects.get(description='Q'+str(i))
+            if Q.indication == "chart-barchart":
+                self.assertEqual(evaluate_chart(Q,self.studentAnswer[i]),self.output[i],self.message[i])
+
+    def testPiechart(self):
+        for i in range(len(self.studentAnswer)):
+            Q = Question.objects.get(description='Q'+str(i))
+            if Q.indication == "chart-piechart":
+                self.assertEqual(evaluate_chart(Q,self.studentAnswer[i]),self.output[i],self.message[i])
+
+if __name__ == '__main__':
+	unittest.main()
+
